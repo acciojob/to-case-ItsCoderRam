bce4c7a760ee7bc9fa5a7a0957cb5cc4684ec7cc
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-09-07 16:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,21 @@ function toCase(text) {
 	let uppercase = "";
 	for (let i = 0; i < text.length; i++) {
 		let charcode = text.charCodeAt(i);
-		if(charcode >=97 &&charcode =< 122 ){
+		if(charcode >=97 && charcode <= 122 ){
 		let str = String.fromCharCode(charcode-32);
 		uppercase = uppercase + str; 
-		}
-		else if(charcode >=65 && charcode <=92){
+		} 
+		else if(charcode >= 65 && charcode <=122){
 		 str = String.fromCharCode(charcode);
 		 uppercase = uppercase + str; 
 		}  
 	}  
 	uppercase = uppercase.slice(1,-1);
 	text = text.slice(1,-1);
-	return `'${text}-${uppercase}`; 
+	return `'${text}-${uppercase}'`; 
+}
 }
 
-// DO not change the code below
-
- 
+// DO not change the code below 
 const text = prompt("Enter text:"); 
 alert(toCase(text)); 
