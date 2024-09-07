ae3595b8bf86cad441cdfded2655228c9ca3aa1c
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-09-08 00:47:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 
 function toCase(text) { 
-	let lowercase = "";
-    let uppercase = "";
-	let lowerstr; 
-	for (let i = 0; i < text.length; i++) { 
-        let charcode = text.charCodeAt(i);
-		if(charcode >=97 && charcode <= 122 ){
-            let str = String.fromCharCode(charcode-32);
-			 lowerstr = String.fromCharCode(charcode); 
-            uppercase = uppercase + str; 
-			lowercase = lowercase + lowerstr;
-        } 
-		else if(charcode >= 65 && charcode <=90){
-             str = String.fromCharCode(charcode);
-			lowerstr = String.fromCharCode(charcode+32);
-            uppercase = uppercase + str;   
-			lowercase = lowercase + lowerstr; 
-        }   
+	// let lowercase = "";
+ //    let uppercase = "";
+	// let lowerstr; 
+	// for (let i = 0; i < text.length; i++) { 
+ //        let charcode = text.charCodeAt(i);
+	// 	if(charcode >=97 && charcode <= 122 ){
+ //            let str = String.fromCharCode(charcode-32);
+	// 		 lowerstr = String.fromCharCode(charcode); 
+ //            uppercase = uppercase + str; 
+	// 		lowercase = lowercase + lowerstr;
+ //        } 
+	// 	else if(charcode >= 65 && charcode <=90){
+ //             str = String.fromCharCode(charcode);
+	// 		lowerstr = String.fromCharCode(charcode+32); 
+ //            uppercase = uppercase + str;   
+	// 		lowercase = lowercase + lowerstr; 
+ //        }   
 		
-    }  
-    return `'${lowercase}-${uppercase}'`; 
+ //    }  
+ //    return `'${lowercase}-${uppercase}'`; 
+	 let lowercase = text.toLowerCase();
+    let uppercase = text.toUpperCase();
+    return `${lowercase}-${uppercase}`;
+
 }
 // DO not change the code below 
  const text = prompt("Enter text:");    
