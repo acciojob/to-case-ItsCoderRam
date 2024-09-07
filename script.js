332ba4c129964416373b694b3a34716bd819b056
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-09-07 13:29:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,15 @@ function toCase(text) {
 	let uppercase = '';
 	for (let i = 0; i < text.length; i++) {
 		let charcode = text.charCodeAt(i);
-		console.log(charcode);
 		if(charcode >=92){
 		let str = String.fromCharCode(charcode-32);
 		uppercase = uppercase + str;
-		console.log(str);
 		}
 		else{
-		  
 		 str = String.fromCharCode(charcode);
 		 uppercase = uppercase + str; 
-		 console.log(str);
 		}
-	} 
+	}  
 	return uppercase;
 }
 
